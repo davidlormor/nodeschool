0a44253fd1486c1a9f844b1121b135bf62adf357
David Lormor <dlormor@aidt.edu>
2014-12-19 14:08:53 -0600

    Complete http time parser

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,15 +1,53 @@
 var http = require('http');
-var map = require('through2-map');
+var url = require('url');
+var moment = require('moment');
 
 var port = process.argv[2];
 
+// Return JSON with hour, minute, and second
+function parseTime(time) {
+  return {
+    'hour': time.hour(),
+    'minute': time.minute(),
+    'second': time.second()
+  };
+}
+
+//Return JSON with unixtime (including milliseconds)
+function unixTime(time) {
+  return {'unixtime': time.valueOf()};
+}
+
+// Respond with error
+function errorResponse(res, method, message) {
+  res.writeHead(method);
+  res.end(message);
+}
+
 var server = http.createServer(function (req, res) {
-  if (req.method != 'POST') {
-    return res.end('POSTs only!');
+  // Parse URL
+  var parsedUrl = url.parse(req.url, true);
+  var pathname = parsedUrl.pathname;
+  var time = moment(parsedUrl.query.iso);
+  var result;
+
+  // Only respond to GET requests
+  if (req.method === 'GET') {
+    if (pathname === '/api/parsetime') {
+      result = parseTime(time);
+    }
+    if (pathname === '/api/unixtime') {
+      result = (unixTime(time));
+    }
+    if (result) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(result));
+    } else {
+      errorResponse(res, 404, 'Path not valid');
+    }
+  } else {
+    errorResponse(res, 404, 'Please send a GET request');
   }
-  req.pipe(map(function (stream) {
-    return stream.toString().toUpperCase();
-  })).pipe(res);
 });
 
 server.listen(port);
