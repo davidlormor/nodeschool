d8b4843e110a77f1d7ea758a4b86502eeb452780
David Lormor <dlormor@aidt.edu>
2014-12-19 13:08:14 -0600

    Complete http upcaserer

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,11 +1,15 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map');
 
 var port = process.argv[2];
-var file = process.argv[3];
 
 var server = http.createServer(function (req, res) {
-  fs.createReadStream(file).pipe(res);
+  if (req.method != 'POST') {
+    return res.end('POSTs only!');
+  }
+  req.pipe(map(function (stream) {
+    return stream.toString().toUpperCase();
+  })).pipe(res);
 });
 
 server.listen(port);
