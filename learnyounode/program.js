e606c04d3f77f7b3004bfbbcf62ca644c8338cae
David Lormor <dlormor@aidt.edu>
2014-12-19 10:36:56 -0600

    Complete http collect

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,10 +1,13 @@
 var http = require('http');
+var concat = require('concat-stream');
 
 var url = process.argv[2];
 
 http.get(url, function (res) {
-  res.setEncoding('utf8');
-  var data = res.on('data', function (data) {
-    console.log(data);
-  });
+  res.on('error', console.error);
+  res.pipe(concat(function (data) {
+    var string = data.toString();
+    console.log(string.length);
+    console.log(string);
+  }));
 });
