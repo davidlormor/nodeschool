0c16d1f3f3908a6c81799737b8c8fa02559f7f4b
David Lormor <dlormor@aidt.edu>
2014-12-18 19:40:58 -0600

    Complete my first async i/o

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 
-var path = process.argv[2];
-
-var file = fs.readFileSync(path);
-console.log(file.toString().split('\n').length - 1);
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  console.log(data.split('\n').length - 1);
+});
