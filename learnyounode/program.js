2679e6191537667d5047b7b0818e50174489623f
David Lormor <dlormor@aidt.edu>
2014-12-19 08:53:56 -0600

    Complete filtered ls

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,5 +1,14 @@
 var fs = require('fs');
+var path = require('path');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-  console.log(data.split('\n').length - 1);
+var filePath = process.argv[2];
+var filter = process.argv[3];
+
+fs.readdir(filePath, function (err, list) {
+  var filtered = list.filter(function (file) {
+    return path.extname(file).slice(1) === filter;
+  });
+  filtered.forEach(function (file) {
+    console.log(file);
+  });
 });
