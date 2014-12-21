e5b19f01d23a516bc0b75436f26323822be7badd
David Lormor <david.lormor@gmail.com>
2014-12-20 22:50:50 -0600

    Complete lines

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,8 +1,18 @@
 var through = require('through');
+var split = require('split');
 var tr = through(write);
+var line = 0;
 
 function write(data) {
-  this.queue(data.toString().toUpperCase());
+  line++;
+  var string = data.toString();
+  this.queue(line % 2 === 0 ?
+    string.toUpperCase() + '\n' :
+    string.toLowerCase() + '\n'
+  );
 }
 
-process.stdin.pipe(tr).pipe(process.stdout);
+process.stdin
+  .pipe(split())
+  .pipe(tr)
+  .pipe(process.stdout);
