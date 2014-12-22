1a6a41227fb9fb2335d2a4e59ae8b6515b88db61
David Lormor <dlormor@aidt.edu>
2014-12-22 11:10:40 -0600

    Complete duplexer redux

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,7 +1,16 @@
 var duplex = require('duplexer');
-var spawn = require('child_process').spawn;
+var through = require('through');
 
-module.exports = function(cmd, args) {
-  var proc = spawn(cmd, args);
-  return duplex(proc.stdin, proc.stdout);
+module.exports = function(counter) {
+  var countries = {};
+  var countryStream = through(write, end);
+
+  function write(location) {
+    countries[location.country] = (countries[location.country] || 0) + 1;
+  }
+  function end () {
+    counter.setCounts(countries);
+  }
+
+  return duplex(countryStream, counter);
 };
