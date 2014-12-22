8ec42d917c98d481ebe3bb61328788c2bdd1dd07
David Lormor <dlormor@aidt.edu>
2014-12-22 14:02:23 -0600

    Complete crypt

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,31 +1,8 @@
-var combine = require('stream-combiner');
-var through = require('through');
-var split = require('split');
-var zlib = require('zlib');
+var crypto = require('crypto');
 
-module.exports = function() {
-  var group = through(write, end);
-  var genre;
+var passphrase = process.argv[2];
+var decrypter = crypto.createDecipher('aes256', passphrase);
 
-  function write (object) {
-    if(object.length === 0) { return; }
-    var row = JSON.parse(object);
-    if(row.type === 'genre') {
-      if(genre) {
-        this.queue(JSON.stringify(genre) + '\n');
-      }
-      genre = { name: row.name, books: [] };
-    } else if (row.type === 'book') {
-      genre.books.push(row.name);
-    }
-  }
-
-  function end() {
-    if(genre) {
-      this.queue(JSON.stringify(genre) + '\n');
-    }
-    this.queue(null);
-  }
-
-  return combine(split(), group, zlib.createGzip());
-};
+process.stdin
+  .pipe(decrypter)
+  .pipe(process.stdout);
