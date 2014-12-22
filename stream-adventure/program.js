984e59ce0e1ad8efcd88bc6b541a8df51c27a2a6
David Lormor <dlormor@aidt.edu>
2014-12-22 15:39:02 -0600

    Complete secretz and finish stream-adventure

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,8 +1,31 @@
+//Modules
 var crypto = require('crypto');
+var tar = require('tar');
+var zlib = require('zlib');
+var through = require('through');
 
-var passphrase = process.argv[2];
-var decrypter = crypto.createDecipher('aes256', passphrase);
+// File decryption
+var cipher = process.argv[2];
+var passphrase = process.argv[3];
+var decrypter = crypto.createDecipher(cipher, passphrase);
+
+// File unzipping
+var unzip = zlib.createGunzip();
+
+// Tarball parsing
+var parser = tar.Parse();
+parser.on('entry', function(e) {
+  if (e.type !== 'File') { return; }
+
+  var hasher = crypto.createHash('md5', { encoding: 'hex' });
+  e.pipe(hasher).pipe(through(null, end)).pipe(process.stdout);
+
+  function end() {
+    this.queue(' ' + e.path + '\n');
+  }
+});
 
 process.stdin
   .pipe(decrypter)
-  .pipe(process.stdout);
+  .pipe(unzip)
+  .pipe(parser);
