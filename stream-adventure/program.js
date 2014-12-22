8ee53a6be79252f6fcce43ee50797046177dcbd6
David Lormor <dlormor@aidt.edu>
2014-12-22 09:46:24 -0600

    Complete http client

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,15 +1,5 @@
-var http = require('http');
-var through = require('through');
-var port = parseInt(process.argv[2]);
+var request = require('request');
 
-var server = http.createServer(function (req, res) {
-  if (req.method === 'POST') {
-    req.pipe(through(function (buf) {
-      this.queue(buf.toString().toUpperCase());
-    })).pipe(res);
-  } else {
-    res.end('Send a POST\n');
-  }
-});
-
-server.listen(port);
+process.stdin
+  .pipe(request.post('http://localhost:8000'))
+  .pipe(process.stdout);
