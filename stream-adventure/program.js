b28f4e34cccc174ac36d6ed81b9cfb7456598213
David Lormor <dlormor@aidt.edu>
2014-12-22 13:55:40 -0600

    Complete combiner

--- stream-adventure/program.js
+++ stream-adventure/program.js
@@ -1,16 +1,31 @@
-var duplex = require('duplexer');
+var combine = require('stream-combiner');
 var through = require('through');
+var split = require('split');
+var zlib = require('zlib');
 
-module.exports = function(counter) {
-  var countries = {};
-  var countryStream = through(write, end);
+module.exports = function() {
+  var group = through(write, end);
+  var genre;
 
-  function write(location) {
-    countries[location.country] = (countries[location.country] || 0) + 1;
+  function write (object) {
+    if(object.length === 0) { return; }
+    var row = JSON.parse(object);
+    if(row.type === 'genre') {
+      if(genre) {
+        this.queue(JSON.stringify(genre) + '\n');
+      }
+      genre = { name: row.name, books: [] };
+    } else if (row.type === 'book') {
+      genre.books.push(row.name);
+    }
   }
-  function end () {
-    counter.setCounts(countries);
+
+  function end() {
+    if(genre) {
+      this.queue(JSON.stringify(genre) + '\n');
+    }
+    this.queue(null);
   }
 
-  return duplex(countryStream, counter);
+  return combine(split(), group, zlib.createGzip());
 };
